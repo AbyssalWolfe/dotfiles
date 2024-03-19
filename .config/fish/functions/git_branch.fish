654761f07e0ef673e44fe4e6c3e8ed35b0999419
Abyssal Wolfe <wolfe@the.abyssal.one>
2024-03-19 16:36:12 -0400

    Updated git config
    Added 'gposu' as alias for 'git push --set-upstream origin $(git_branch current)'
    Updated 'git_branch' to use the function 'git' instead of command 'git'
    
    Signed-off-by: Abyssal Wolfe <wolfe@the.abyssal.one>

--- .config/fish/functions/git_branch.fish
+++ .config/fish/functions/git_branch.fish
@@ -7,22 +7,22 @@
 
 # Get current/master/develop branch
 function git_branch -d "Get name of [current|master|develop] branch"
-	command git rev-parse --git-dir &>/dev/null || return
+	git rev-parse --git-dir &>/dev/null || return
 	switch $argv[1]
 		case current
-			set ref (command git symbolic-ref --quiet HEAD 2> /dev/null)
+			set ref (git symbolic-ref --quiet HEAD 2> /dev/null)
 			switch $status
 				case 0
 					echo (string split -r -m1 -f 2 / $ref)
 				case 128
 					return
 				case "*"
-					echo (command git rev-parse --short HEAD 2> /dev/null)
+					echo ( git rev-parse --short HEAD 2> /dev/null)
 			end
 			return
 		case master
 			for ref in refs/{heads,remotes/{origin/upstream}}/{default,main,mainline,master,trunk}
-				if command git show-ref -q --verify $ref
+				if git show-ref -q --verify $ref
 					echo (string split -r -m1 -f 2 / $ref)
 					return
 				end
@@ -30,7 +30,7 @@ function git_branch -d "Get name of [current|master|develop] branch"
 			echo master
 		case develop
 			for branch in dev devel develop development
-				if command git show-ref -q --verify refs/heads/$branch
+				if git show-ref -q --verify refs/heads/$branch
 					echo $branch
 					return
 				end
