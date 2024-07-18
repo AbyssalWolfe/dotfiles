0535a05164c37e2fb86c02017f3542dd99a94a5d
Abyssal Wolfe <wolfe@the.abyssal.one>
2024-07-18 10:09:30 -0400

    Updated Sway & Fish configs
    Switched to WebCord in place of Discord
    
    Signed-off-by: Abyssal Wolfe <wolfe@the.abyssal.one>

--- .config/fish/functions/git.fish
+++ .config/fish/functions/git.fish
@@ -6,10 +6,10 @@
 #  ╚═════╝ ╚═╝   ╚═╝
 
 # If in $HOME then set dotfiles dirs
-function git -w "git" -d "git - Version Control System"
-	if test (pwd) = $HOME
-		command git --git-dir=$XDG_DATA_HOME/dotfiles/ --work-tree=$HOME $argv
-	else
-		command git $argv
-	end
+function git -w git -d "git - Version Control System"
+    if test (pwd) = $HOME
+        command git --git-dir=$XDG_DATA_HOME/dotfiles/ --work-tree=$HOME $argv
+    else
+        command git $argv
+    end
 end
