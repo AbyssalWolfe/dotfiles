f040216742916f3f6ae270767102374ccdc1582c
Abyssal Wolfe <wolfe@the.abyssal.one>
2024-12-31 14:48:16 -0500

    Updated LazyVim config
    Copilot category changed from 'coding' to 'ai'.
    
    Signed-off-by: Abyssal Wolfe <wolfe@the.abyssal.one>

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -30,8 +30,8 @@ require("lazy").setup({
 		-- add LazyVim and import its plugins
 		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
 		-- import LazyVim extras
-		{ import = "lazyvim.plugins.extras.coding.copilot" },
-		{ import = "lazyvim.plugins.extras.coding.copilot-chat" },
+		{ import = "lazyvim.plugins.extras.ai.copilot" },
+		{ import = "lazyvim.plugins.extras.ai.copilot-chat" },
 		{ import = "lazyvim.plugins.extras.coding.mini-comment" },
 		{ import = "lazyvim.plugins.extras.coding.mini-surround" },
 		{ import = "lazyvim.plugins.extras.dap.core" },
