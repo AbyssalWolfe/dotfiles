11c7ab3034736bb1902c2497683195433de58d9a
Abyssal Wolfe <wolfe@the.abyssal.one>
2024-12-31 19:09:26 -0500

    Updated Wezterm config
    Removed unnecessary default key binds
    
    Signed-off-by: Abyssal Wolfe <wolfe@the.abyssal.one>

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -13,6 +13,7 @@ local wezterm = require("wezterm")
 
 return {
 	color_scheme = "Dracula PRO",
+	disable_default_key_bindings = true,
 	disable_default_mouse_bindings = true,
 	enable_scroll_bar = false,
 	enable_tab_bar = false,
@@ -23,6 +24,13 @@ return {
 	harfbuzz_features = { "cv02", "ss03", "ss04", "ss05", "ss07", "ss09" },
 	initial_cols = 100,
 	initial_rows = 35,
+	keys = {
+		{ key = "C", mods = "CTRL", action = wezterm.action.CopyTo("Clipboard") },
+		{ key = "C", mods = "SHIFT|CTRL", action = wezterm.action.CopyTo("Clipboard") },
+		{ key = "R", mods = "SHIFT|CTRL", action = wezterm.action.ReloadConfiguration },
+		{ key = "V", mods = "CTRL", action = wezterm.action.PasteFrom("Clipboard") },
+		{ key = "V", mods = "SHIFT|CTRL", action = wezterm.action.PasteFrom("Clipboard") },
+	},
 	text_background_opacity = 0.5,
 	underline_position = -3,
 	underline_thickness = 2,
